061c8841b4ddb4e66bca053d989cdfdd3bd3f7b0
Haris <haristallat01@gmail.com>
2023-07-20 16:26:09 +0500

    Initial Commit

---
+++ haris_checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  haris_checker
+//
+//  Created by Haris Tallat on 20/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
